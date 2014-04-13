a2ea079cd5217c9676752efae9f9cca2fdbf5769
gongyiling <gongyiling@github.com>
2014-04-13 19:53:49 +0800

    add main.lua

---
+++ Resources/scripts/main.lua
@@ -0,0 +1,18 @@
+
+cclog = function(...)
+    print(string.format(...))
+end
+
+-- for CCLuaEngine traceback
+function __G__TRACKBACK__(msg)
+    cclog("----------------------------------------")
+    cclog("LUA ERROR: " .. tostring(msg) .. "\n")
+    cclog(debug.traceback())
+    cclog("----------------------------------------")
+end
+
+function main()
+    print("main called")
+end
+
+xpcall(main, __G__TRACKBACK__)
